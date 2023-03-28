fe76fbf8886e97cc1ceff9fcd4d52d9dc6c337ec
Maksim <komarovmu@gmail.com>
2023-03-29 00:17:08 +0100

    add Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write your name");
+string username=Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+
+
