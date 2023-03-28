65f0b5b734f7cea350eb11631d1b0cee23449034
Maksim <komarovmu@gmail.com>
2023-03-29 00:39:58 +0100

    Example004 Div

---
+++ Example004/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+double NumberA = 12;
+double NumberB = 5;
+double result = NumberA/NumberB;
+Console.WriteLine(result);
