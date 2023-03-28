97be7cf5633154b7e59e0552fde219e93ee1a912
Maksim <komarovmu@gmail.com>
2023-03-29 00:48:09 +0100

    add Random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int NumberA = 31;
-int NumberB = 5;
+int NumberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(NumberA); 
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
 int result = NumberA+NumberB;
 Console.WriteLine(result);
 
