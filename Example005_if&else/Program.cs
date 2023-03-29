48d1308ffd507fd0370f93ce7bcdfda0a3590f3a
Maksim <komarovmu@gmail.com>
2023-03-29 01:00:35 +0100

    Masha

---
+++ Example005_if&else/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите имя пользователя!");
+
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+  {
+    Console.WriteLine("Ura, Masha prishla!");
+  }
+  else
+  {
+    Console.WriteLine("Privet, ");
+    Console.WriteLine("ti ne Masha!");
+  }
